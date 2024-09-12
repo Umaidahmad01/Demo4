daec8ad919a6d5ff39e31d5bccacd6a8af30214e
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-09-12 23:04:09 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,13 +10,13 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ ᴏᴡɴᴇʀ : <a href=https://t.me/rin_nanakura'>rin nanakura</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/ongoing_society'>ᴏɴɢᴏɪɴɢ sᴏᴄɪᴇᴛʏ</a>\n○ ᴀɴɪᴍᴇ sᴜʙ sᴏᴄɪᴇᴛʏ : <a href='https://t.me/anime_sub_society'>ᴀɴɪᴍᴇ sᴜʙ sᴏᴄɪᴇᴛʏ</a>\n○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/AHSS_HELP_ZONE'>sᴏᴄɪᴇᴛʏ ᴄʜᴀᴛ ᴢᴏɴᴇ</a>\n○ Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ : <a href='https://t.me/i_killed_my_clan'>๏ 𝐎ʙɪᴛᴏ ᴜᴄʜɪʜᴀ ๏</a></b>",
+            text = f"<b>⌬ ᴏᴡɴᴇʀ : <a href=https://t.me/WoW_Gameplayes_YT>ᴀᴅɪᴛɪʏᴀ</a>\n⌬ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/animes_in_hindi_sub'>ᴀɴɪᴍᴇ ʜs</a>\n⌬ ᴀɴɪᴍᴇ ʜs : <a href='https://t.me/animes_in_hindi_sub'></a>\n⌬ ᴀɴɪᴍᴇ ʜs ᴄʜᴀᴛ : <a href='https://t.me/+xgMSJVXJnyM1MDY1'>ᴄʜᴀᴛ ᴢᴏɴᴇ</a>\n࿂ Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ : <a href='https://t.me/i_killed_my_clan'>❰⏤͟͞ 𝚯𝗕𝗜𝗧𝗢 -//-❱</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
                     InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data = "close"),
-                    InlineKeyboardButton('⛩️ ʜᴏᴍᴇ ⛩️ ', url='https://t.me/Anime_sub_society')
+                    InlineKeyboardButton('⛩️ ʜᴏᴍᴇ ⛩️ ', url='https://t.me/animes_in_hindi_sub')
                     ]
                 ]
             )
