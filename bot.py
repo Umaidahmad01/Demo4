1f611eb01daae5eeb6a8305ef91d057f8efedf09
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-09-20 21:43:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE
 
 
 name ="""
- BY MIKEY FROM TG
+ BY OBITO FROM TG
 """
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
