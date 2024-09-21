e4b43762b1847781155f4aadcd207f1e9e701d45
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-09-21 11:32:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,10 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Umaid:umaid@cluster0.k2yx
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002033058072 -1002126461167 -1002072642438').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+#force sub channel id, if you want enable force sub
+FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1002033058072"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002126461167"))
+FORCESUB_CHANNEL3 = int(os.environ.get("FORCESUB_CHANNEL3", "-1002072642438"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
